70a231493d7c86f6703ecc87b28dc905774b8884
thanhPM <thanhPM210@gmail.com>
2026-08-31 09:04:49 +0700

    add alias for rebuild nixos easier

---
+++ files/prompt.zsh
@@ -0,0 +1,97 @@
+# Converted from shared/prompt.zsh + Darwin/prompt.zsh + Linux/prompt.zsh
+# (merged: the only real OS difference was the tput/SYN_HIGH_PATH check,
+# which works fine unified across both platforms; syntax highlighting is
+# now handled natively by programs.zsh.syntaxHighlighting in zsh.nix)
+
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if command -v tput >/dev/null && tput setaf 1 >/dev/null 2>&1; then
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+# set variable identifying the chroot you work in (Linux only; harmless no-op on macOS)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+configure_prompt() {
+    prompt_symbol=🤫
+    case "$PROMPT_ALTERNATIVE" in
+        twoline)
+            PROMPT=$'%F{%(#.blue.green)}┌──${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV_PROMPT:+($VIRTUAL_ENV_PROMPT)-}(%B%F{%(#.red.blue)}%n'$prompt_symbol$'%m%b%F{%(#.blue.green)})-[%B%F{reset}%(6~.%-1~/…/%4~.%5~)%b%F{%(#.blue.green)}]$branch\n└─%B%(#.%F{red}#.%F{blue}$)%b%F{reset} '
+            ;;
+        oneline)
+            PROMPT=$'${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%B%F{%(#.red.blue)}%n@%m%b%F{reset}:%B%F{%(#.blue.green)}%~%b%F{reset}%(#.#.$) '
+            RPROMPT=
+            ;;
+        backtrack)
+            PROMPT=$'${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%B%F{red}%n@%m%b%F{reset}:%B%F{blue}%~%b%F{reset}%(#.#.$) '
+            RPROMPT=
+            ;;
+    esac
+    unset prompt_symbol
+}
+
+# START KALI CONFIG VARIABLES
+if [ "$XDG_SESSION_TYPE" = "tty" ]; then
+    PROMPT_ALTERNATIVE=oneline
+else
+    PROMPT_ALTERNATIVE=twoline
+fi
+NEWLINE_BEFORE_PROMPT=yes
+# STOP KALI CONFIG VARIABLES
+
+color_prompt=yes
+if [ "$color_prompt" = yes ]; then
+    VIRTUAL_ENV_DISABLE_PROMPT=1
+    configure_prompt
+else
+    PROMPT='${debian_chroot:+($debian_chroot)}%n@%m:%~%(#.#.$) '
+fi
+unset color_prompt force_color_prompt
+
+toggle_oneline_prompt(){
+    if [ "$PROMPT_ALTERNATIVE" = oneline ]; then
+        PROMPT_ALTERNATIVE=twoline
+    else
+        PROMPT_ALTERNATIVE=oneline
+    fi
+    configure_prompt
+    zle reset-prompt
+}
+zle -N toggle_oneline_prompt
+bindkey ^P toggle_oneline_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*|Eterm|aterm|kterm|gnome*|alacritty)
+    TERM_TITLE=$'\e]0;${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%n@%m: %~\a'
+    ;;
+*)
+    ;;
+esac
+
+precmd() {
+    print -Pnr -- "$TERM_TITLE"
+
+    if [ "$NEWLINE_BEFORE_PROMPT" = yes ]; then
+        if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
+            _NEW_LINE_BEFORE_PROMPT=1
+        else
+            print ""
+        fi
+    fi
+
+    if branch=$(git branch --show-current 2>/dev/null); then
+        branch='-('%B%F{%(#.blue.red)}$branch%b%F{%(#.blue.green)}')'
+    else
+        branch=''
+    fi
+}
--- zsh.nix
+++ zsh.nix
@@ -1,10 +1,73 @@
 { ... }:
 {
     programs.zsh = {
-    enable = true;
-    completionInit = ''
-      source $HOME/.zconfig/main.zsh
-    '';
-    syntaxHighlighting.enable = true;
-  };
+        enable = true;
+        completionInit = ''
+            #option
+            setopt autocd              # change directory just by typing its name
+            #setopt correct            # auto correct mistakes
+            setopt interactivecomments # allow comments in interactive mode
+            setopt magicequalsubst     # enable filename expansion for arguments of the form ‘anything=expression’
+            setopt nonomatch           # hide error message if there is no match for the pattern
+            setopt notify              # report the status of background jobs immediately
+            setopt numericglobsort     # sort filenames numerically when it makes sense
+            setopt promptsubst         # enable command substitution in prompt
+
+            WORDCHARS='_-' # Don't consider certain characters part of the word
+
+            # hide EOL sign ('%')
+            PROMPT_EOL_MARK=""
+
+            # configure key keybindings
+            bindkey -e                                        # emacs key bindings
+            bindkey ' ' magic-space                           # do history expansion on space
+            bindkey '^U' backward-kill-line                   # ctrl + U
+            bindkey '^[[3;5~' kill-word                       # ctrl + Supr
+            bindkey '^[[3~' delete-char                       # delete
+            bindkey '^[[1;5C' forward-word                    # ctrl + ->
+            bindkey '^[[1;5D' backward-word                   # ctrl + <-
+            bindkey '^[[5~' beginning-of-buffer-or-history    # page up
+            bindkey '^[[6~' end-of-buffer-or-history          # page down
+            bindkey '^[[H' beginning-of-line                  # home
+            bindkey '^[[F' end-of-line                        # end
+            bindkey '^[[Z' undo                               # shift + tab undo last action
+
+            # enable completion features
+            autoload -Uz compinit
+            compinit -d ~/.cache/zcompdump
+            zstyle ':completion:*:*:*:*:*' menu select
+            zstyle ':completion:*' auto-description 'specify: %d'
+            zstyle ':completion:*' completer _expand _complete
+            zstyle ':completion:*' format 'Completing %d'
+            zstyle ':completion:*' group-name '''
+            zstyle ':completion:*' list-colors '''
+            zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            zstyle ':completion:*' rehash true
+            zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+            zstyle ':completion:*' use-compctl false
+            zstyle ':completion:*' verbose true
+            zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+            # configure `time` format
+            TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S\ncpu\t%P'
+
+
+            #source $HOME/.zconfig/main.zsh
+        '';
+        syntaxHighlighting.enable = true;
+        history = {
+            size = 50000;              # HISTSIZE - entries kept in memory
+            save = 50000;              # SAVEHIST - entries saved to file
+            path = "~/.zsh_history";  # HISTFILE location
+            ignoreDups = true;         # setopt HIST_IGNORE_DUPS
+            ignoreAllDups = true;      # setopt HIST_IGNORE_ALL_DUPS
+            ignoreSpace = true;        # setopt HIST_IGNORE_SPACE (don't save cmds starting with space)
+            expireDuplicatesFirst = true;  # setopt HIST_EXPIRE_DUPS_FIRST
+            share = true;              # setopt SHARE_HISTORY (share across sessions in real-time)
+            extended = true;           # setopt EXTENDED_HISTORY (save timestamps)
+            append = true;             # setopt APPEND_HISTORY
+            findNoDups = true;         # setopt HIST_FIND_NO_DUPS
+        };
+    };
 }
